2ecbafa779ffa1eb808ab1df652c05eab6a9d659
Simon Heath <icefoxen@gmail.com>
2018-04-30 13:21:47 -0400

    Added queue methods to TextCached (experimental)

--- src/graphics/text_cached.rs
+++ src/graphics/text_cached.rs
@@ -28,6 +28,78 @@ impl TextCached {
             "`TextCached` can only be used with a `Font::GlyphFont`!".into(),
         ))
     }
+
+    /// Queues text for drawing.
+    pub fn queue(&self, ctx: &mut Context) {
+        let (font_id, font_scale) = (self.font_id, self.font_scale);
+        ctx.gfx_context.glyph_brush.queue(Section {
+            text: &self.contents,
+            //screen_position: (dest.x, dest.y),
+            //bounds: (f32, f32),
+            scale: font_scale,
+            color: WHITE.into(),
+            //z: f32,
+            //layout: Layout<BuiltInLineBreaker>,
+            font_id,
+            ..Section::default()
+        });
+    }
+
+    /// Draw all queued text.
+    pub fn draw_queued(&self, ctx: &mut Context, param: DrawParam) -> GameResult<()> {
+        type Mat4 = na::Matrix4<f32>;
+        type Vec3 = na::Vector3<f32>;
+
+        let (offset_x, offset_y) = (-1.0, 1.0);
+        let (screen_w, screen_h) = (ctx.gfx_context.screen_rect.w, ctx.gfx_context.screen_rect.h);
+        let (aspect, aspect_inv) = (screen_h / screen_w, screen_w / screen_h);
+        let m_aspect = Mat4::new_nonuniform_scaling(&Vec3::new(1.0, aspect_inv, 1.0));
+        let m_aspect_inv = Mat4::new_nonuniform_scaling(&Vec3::new(1.0, aspect, 1.0));
+        let m_scale = Mat4::new_nonuniform_scaling(&Vec3::new(param.scale.x, param.scale.y, 1.0));
+        let m_shear = Mat4::new(
+            1.0,
+            param.shear.x,
+            0.0,
+            0.0,
+            param.shear.y,
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        );
+        let m_rotation = Mat4::new_rotation(param.rotation * Vec3::z());
+        let m_offset = Mat4::new_translation(&Vec3::new(offset_x, offset_y, 0.0));
+        let m_offset_inv = Mat4::new_translation(&Vec3::new(-offset_x, -offset_y, 0.0));
+        let m_translate = Mat4::new_translation(&Vec3::new(
+            param.dest.x / screen_w,
+            -param.dest.y / screen_h,
+            0.0,
+        ));
+
+        let m_transform = m_translate * m_offset * m_aspect * m_rotation * m_scale * m_shear
+            * m_aspect_inv * m_offset_inv;
+
+        let (encoder, render_tgt, depth_view) = (
+            &mut ctx.gfx_context.encoder,
+            &ctx.gfx_context.screen_render_target,
+            &ctx.gfx_context.depth_view,
+        );
+
+        ctx.gfx_context.glyph_brush.draw_queued_with_transform(
+            m_transform.into(),
+            encoder,
+            render_tgt,
+            depth_view,
+        )?;
+        Ok(())
+    }
 }
 
 impl Drawable for TextCached {
